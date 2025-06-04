05d19482d35e8eaf346dac4c30098aa2eee375d8
Charles Haines <charlesmhainesii@gmail.com>
2025-06-04 07:22:01 -0400

    added assets and animations for character

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 139.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 95.0
+const JUMP_VELOCITY = -275.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
@@ -19,21 +19,28 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("walk_left", "walk_right")
 	
+	# Flip the sprite if needed
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
 	# Play animation
-	if direction == 0:
-		animated_sprite.play("idle")
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("walk")
 	else:
-		animated_sprite.play("walk")
+		animated_sprite.play("jump")
 	
 	# Apply Movment
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if global_position.y >= 0:
+		get_tree().reload_current_scene()
 
 	move_and_slide()
