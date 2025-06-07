7d94ebff9083b6a9fedb12c9f81de8e025c2097a
Charles Haines <charlesmhainesii@gmail.com>
2025-06-07 03:02:21 -0400

    added ladder logic

---
+++ scenes/spark_man.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+@onready var layer_1: TileMapLayer = $layer1
+@onready var layer_2: TileMapLayer = $layer2
+@onready var flicker_timer: Timer = $FlickerTimer
+
+
+var flicker_state = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	flicker_timer.wait_time = 0.071 # 6 frams at 60fps
+	flicker_timer.timeout.connect(_on_flicker_timer_timeout)
+	flicker_timer.start()
+	
+func _on_flicker_timer_timeout():
+	flicker_state = !flicker_state
+	layer_1.visible = flicker_state
+	layer_2.visible = !flicker_state
+	
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,58 @@
 extends CharacterBody2D
 
 
-const SPEED = 95.0
+const SPEED = 70.0
 const JUMP_VELOCITY = -275.0
+const CLIMB_SPEED = 60
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+var on_ladder = false
+var can_grab_ladder = false
+var ladder_area : Area2D = null
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func _ready() -> void:
+	for ladder in get_tree().get_nodes_in_group("ladder"):
+		ladder.connect("body_entered", Callable(self, "_on_ladder_body_entered").bind(ladder))
+		ladder.connect("body_exited", Callable(self, "_on_ladder_body_exited").bind(ladder))
+
+
+func _physics_process(delta: float) -> void:
+	var up := Input.is_action_pressed("move_up")
+	var down := Input.is_action_pressed("move_down")
+	
+	# Ladder: Enter logic
+	if not on_ladder and can_grab_ladder and up:
+		enter_ladder()
+	
+	# Ladder climbing behavior
+	if on_ladder:
+		# Climbing movement
+		if up:
+			velocity.y = -CLIMB_SPEED
+		elif down:
+			velocity.y = CLIMB_SPEED
+		else:
+			velocity.y = 0
+			
+		# Exit ladder if jump is pressed
+		if Input.is_action_just_pressed("jump"):
+			exit_ladder()
+			velocity.y = -JUMP_VELOCITY
+			
+		
+	elif is_on_floor() and down and not on_ladder and can_grab_ladder and ladder_area:
+		print("Entering ladder from top")
+		global_position.y += 5
+		enter_ladder()
+	else:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,7 +65,13 @@ func _physics_process(delta: float) -> void:
 		animated_sprite.flip_h = true
 		
 	# Play animation
-	if is_on_floor():
+	if on_ladder:
+		if up or down:
+			animated_sprite.play("climb")
+		else:
+			animated_sprite.pause()
+		
+	elif is_on_floor():
 		if direction == 0:
 			animated_sprite.play("idle")
 		else:
@@ -40,7 +85,35 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	if global_position.y >= 0:
+	if global_position.y >= 120 :
 		get_tree().reload_current_scene()
 
 	move_and_slide()
+
+
+
+
+func exit_ladder():
+	on_ladder = false
+	
+func enter_ladder():
+	on_ladder = true
+	velocity = Vector2.ZERO
+	if ladder_area:
+		global_position.x = ladder_area.global_position.x
+
+
+func _on_ladder_body_entered(body: Node2D, ladder: Area2D) -> void:
+	if body == self:
+		print("Entered ladder area")
+		can_grab_ladder = true
+		ladder_area = ladder
+
+
+func _on_ladder_body_exited(body: Node2D, ladder: Area2D) -> void:
+	if body == self and ladder_area == ladder:
+		can_grab_ladder = false
+		if on_ladder:
+			exit_ladder()
+		ladder_area = null
+		print("Exited ladder")
